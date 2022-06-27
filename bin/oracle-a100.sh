3b413bb99d8bf9156dbd6d1bb6d171a0a9e7ca54
Amit Parekh <7276308+amitkparekh@users.noreply.github.com>
2022-06-27 10:16:45 +0100

    Init pyenv-virtualenv within the .bashrc (#1)

--- bin/oracle-a100.sh
+++ bin/oracle-a100.sh
@@ -10,6 +10,7 @@ curl https://pyenv.run | bash
 	echo 'export PYENV_ROOT="$HOME/.pyenv"'
 	echo 'command -v pyenv >/dev/null || export PATH="$PYENV_ROOT/bin:$PATH"'
 	echo 'eval "$(pyenv init -)"'
+	echo 'eval "$(pyenv virtualenv-init -)"'
 } >>~/.bashrc
 
 # --------------------- Install Python build dependencies -------------------- #
